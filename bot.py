0e58c893a49bea5a3c6a57764f37b86ea11b8b76
kingtz3361 <99616895+kingtz3361@users.noreply.github.com>
2025-12-24 10:54:24 -0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,40 @@
+from telegram.ext import Updater, MessageHandler, Filters
+
+TOKEN = "توکن_ربات_اینجا"
+ADMIN_CHAT_ID = 239467720  # آیدی خودت
+
+def user_message(update, context):
+    user = update.message.from_user
+    text = update.message.text
+
+    msg = f"""
+پیام جدید 📩
+
+👤 نام: {user.first_name}
+🆔 یوزرنیم: @{user.username}
+🆔 آیدی: {user.id}
+
+💬 پیام:
+{text}
+"""
+    context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=msg)
+
+def admin_reply(update, context):
+    if update.message.reply_to_message:
+        replied = update.message.reply_to_message.text
+        for line in replied.split("\n"):
+            if "🆔 آیدی:" in line:
+                user_id = int(line.split(":")[1].strip())
+                context.bot.send_message(
+                    chat_id=user_id,
+                    text=update.message.text
+                )
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(MessageHandler(Filters.text & ~Filters.command, user_message))
+dp.add_handler(MessageHandler(Filters.text & Filters.reply, admin_reply))
+
+updater.start_polling()
+updater.idle()
